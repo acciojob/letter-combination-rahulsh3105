40d2a7accdfeffe625454f3b32b02afc979e7e5e
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-16 14:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,37 @@
-var lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
+function letterCombinations(digits) {
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
 
-function rot13(encodedStr) {
-  var words = encodedStr.split(" "); // String to Array
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    let decoded_word = "";
-    for (let j = 0; j < word.length; j++) {
-      var char = word.charAt(j);
-      var decoded_char = lookup[char];
-      decoded_word += decoded_char;
-    }
-    decodedArr.push(decoded_word);
+  if (digits.length === 0) {
+    return [];
   }
-  return decodedArr.join(" ");
+
+  const result = [];
+
+  function backtrack(currentString, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(currentString);
+      return;
+    }
+
+    const letters = digitToLetters[nextDigits[0]];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(currentString + letter, nextDigits.slice(1));
+    }
+  }
+
+  backtrack('', digits);
+  return result.sort();
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
+module.exports = letterCombinations;
